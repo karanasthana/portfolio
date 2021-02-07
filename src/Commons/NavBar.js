8fd5873084080ac072731d4ae4946ee0df8a4774
Karan Asthana <karan@p3infotech.in>
2021-02-07 22:34:15 +0530

    added files

---
+++ src/Commons/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+class NavBar extends React.Component {
+render() {
+      return (
+          <div>
+         <Navbar bg="dark" expand="lg">
+            <Navbar.Brand href="#home">Karan Asthana</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="#blog">Blog</Nav.Link>
+                <Nav.Link href="#blog">Blog</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+         </Navbar>
+        </div>
+      )
+   }
+}
+export default NavBar
