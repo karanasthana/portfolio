54023e765597b566fe349923f1b3733d68ccc4d3
Karan Asthana <karan@p3infotech.in>
2021-02-07 22:34:00 +0530

    minor routing changes and added bootstrap

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-
-import { BrowserRouter } from 'react-router-dom'
 
 const routs = (
    <div>
       <App />
    </div>
 );
-ReactDOM.render(<React.StrictMode>{routs}</React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(routs, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
