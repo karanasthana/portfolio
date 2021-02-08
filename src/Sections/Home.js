134b61c546079da59b12df6ab3b9086e1d589db9
Karan Asthana <karan@p3infotech.in>
2021-02-08 23:52:42 +0530

    commented out the home heading and button

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Button } from '../StyledComponents/Button';
+// import { Button } from '../StyledComponents/Button';
 
 class Home extends React.Component {
    render() {
       return (
          <div>
-            <h1>Home</h1>
-            <Button primary onClick={() => { console.log('Test clicked!')}}>Test</Button>
+            {/* <h1>Home</h1>
+            <Button primary onClick={() => { console.log('Test clicked!')}}>Test</Button> */}
          </div>
       )
    }
