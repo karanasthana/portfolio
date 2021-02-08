2bb835b6106dfddd74593c53e5875e2d13310aff
Karan Asthana <karan@p3infotech.in>
2021-02-08 23:59:19 +0530

    added basic bootstrapping grid on the top of Home

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -4,11 +4,23 @@ import React from 'react';
 class Home extends React.Component {
    render() {
       return (
-         <div>
+         <div className="container">
             {/* <h1>Home</h1>
             <Button primary onClick={() => { console.log('Test clicked!')}}>Test</Button> */}
+            <div className="row">
+               <div className="col-12 col-md-6">
+                  <h1>
+                     Hello, I am Karan
+                  </h1>
+               </div>
+               <div className="col-12 col-md-6">
+                  <h1>
+                     Hello, I am Karan
+                  </h1>
+               </div>
+            </div>
          </div>
       )
    }
 }
-export default Home
+export default Home;
