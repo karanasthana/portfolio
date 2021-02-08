d4ef13cd032f4216f761523b061614b0fe8ca080
Karan Asthana <karan@p3infotech.in>
2021-02-09 00:05:46 +0530

    minor changes

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -8,12 +8,12 @@ class Home extends React.Component {
             {/* <h1>Home</h1>
             <Button primary onClick={() => { console.log('Test clicked!')}}>Test</Button> */}
             <div className="row">
-               <div className="col-12 col-md-6">
+               <div className="col-12 col-md-4">
                   <h1>
-                     Hello, I am Karan
+                     Hi, I am <span style={{ color: 'gold' }}>Karan Asthana</span>
                   </h1>
                </div>
-               <div className="col-12 col-md-6">
+               <div className="col-12 col-md-8">
                   <h1>
                      Hello, I am Karan
                   </h1>
