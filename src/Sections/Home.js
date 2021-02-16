d5d0b8a4e1a2ebc39a0ee1568bf9c3118461f5a8
Karan Asthana <karan@p3infotech.in>
2021-02-17 00:37:49 +0530

    added new library for fontAwesome

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -2,25 +2,30 @@ import React from 'react';
 // import { Button } from '../StyledComponents/Button';
 import Image from 'react-bootstrap/Image';
 import Background from '../assets/white-bg.jpg';
+import Footer from './Footer';
 
 class Home extends React.Component {
    render() {
       return (
-         <div style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover' }}>
-            <div className="container">
-            {/* <h1>Home</h1>
-               <Button primary onClick={() => { console.log('Test clicked!')}}>Test</Button> */}
-               <div className="row">
-                  <div className="col-12 col-md-6" style={{ display: "flex", alignItems: "center" }}>
-                     <h1>
-                        Hi, I am <span style={{ color: 'orange' }}>Karan Asthana</span>
-                     </h1>
-                  </div>
-                  <div className="col-12 col-md-6" style={{ maxHeight: '80vh', display: "flex", justifyContent: 'center' }}>
-                     <Image src="https://travel-photos-react-website.s3.ap-south-1.amazonaws.com/IMG_8447-removebg-preview-removebg-preview.png" fluid />
+         <div>
+            <div style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover' }}>
+               <div className="container">
+                  <div className="row">
+                     <div className="col-12 col-md-6" style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", marginTop: "20px", marginBottom: "20px" }}>
+                        <h1>
+                           Hi, I am <span style={{ color: 'orange' }}>Karan Asthana</span>
+                        </h1>
+                        <h2>
+                           Frontend Developer
+                        </h2>
+                     </div>
+                     <div className="col-12 col-md-6" style={{ maxHeight: '80vh', display: "flex", justifyContent: 'center' }}>
+                        <Image src="https://travel-photos-react-website.s3.ap-south-1.amazonaws.com/IMG_8447-removebg-preview-removebg-preview.png" fluid />
+                     </div>
                   </div>
                </div>
             </div>
+            <Footer />
          </div>
       )
    }
