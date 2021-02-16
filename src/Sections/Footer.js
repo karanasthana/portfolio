22a703d94ea0a3fa646327177a643f6b58a0e810
Karan Asthana <karan@p3infotech.in>
2021-02-17 00:38:30 +0530

    added the Footer js

---
+++ src/Sections/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Button } from '../StyledComponents/Button';
+import Image from 'react-bootstrap/Image';
+import Background from '../assets/white-bg.jpg';
+
+export default class Footer extends React.Component {
+    render() {
+        return <div style={{ backgroundColor: '#F4F5F7', height: '100px' }}>
+            <div className="row"></div>
+            <div style={{ display: "flex", justifyContent: "center", padding: "10px" }}>
+                CopyRight Karan
+            </div>
+        </div>
+    }
+}
