f83f58c842994981bd4aa6c6c04090917361d01f
Karan Asthana <karan@p3infotech.in>
2021-02-18 00:56:08 +0530

    added new section and its styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FirstFoldAboutKaran from '../Sections/FirstFoldAboutKaran';
 import Footer from '../Sections/Footer';
+import Projects from '../Sections/Projects';
 
 class Home extends React.Component {
    render() {
       return (
          <div>
             <FirstFoldAboutKaran />
+            <Projects />
             <Footer />
          </div>
       )
