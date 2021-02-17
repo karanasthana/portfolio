396b37837a9a51f6ccbb192cf5bea406ca144509
Karan Asthana <karan@p3infotech.in>
2021-02-18 00:56:21 +0530

    added new section file

---
+++ src/Sections/Projects.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './sass.scss';
+
+class Projects extends React.Component {
+   render() {
+      return (
+         <div className="projects-section">
+            <div className="row">
+                <div className="col-12">
+                    <h2>Journey</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-6">
+
+                </div>
+                <div className="col-12 col-md-6">
+
+                </div>
+            </div>
+         </div>
+      )
+   }
+}
+export default Projects;
