350f41655c3b387b0d726232619f7d9f68cc869f
Karan Asthana <karan@p3infotech.in>
2021-02-17 23:19:44 +0530

    fixed css in footer too

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import { Button } from '../StyledComponents/Button';
+import './sass.scss';
 
 export default class Footer extends React.Component {
     render() {
-        return <div style={{ backgroundColor: '#F4F5F7' }}>
-            <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+        return <div className="footer-section">
+            <div className="pdf-row">
                 <Button primary>
-                    <a href="https://karanpersonal.s3.ap-south-1.amazonaws.com/Karan+Asthana+Resume+-+Frontend+Developer+-+React+Native+.pdf" target="_blank" rel="noreferrer" download style={{ color: 'white', textDecoration: 'none' }}>
+                    <a href="https://karanpersonal.s3.ap-south-1.amazonaws.com/Karan+Asthana+Resume+-+Frontend+Developer+-+React+Native+.pdf" target="_blank" rel="noreferrer" download style={{  }}>
                         PDF Resume
                     </a>
                 </Button>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-                <div style={{ display: "flex", justifyContent: "center" }}>
+            <div className="footer-icons-row">
+                <div className="copyright-container">
                     Copyright <i class ="ri-copyright-line" /> {' Karan Asthana'} All rights reserved.
                 </div>
-                <div className="row" style={{ justifyContent: 'center' }}>
-                    <a href={'https://karanasthana.github.io/travel/'} style={{ textDecoration: 'none', color: 'silver', padding: '5px', fontSize: '20px' }} target="_blank" rel="noreferrer">
+                <div className="row icons-container">
+                    <a href={'https://karanasthana.github.io/travel/'} target="_blank" rel="noreferrer">
                         <i class="ri-earth-fill"></i>
                     </a>
-                    <a href={'https://www.linkedin.com/in/karan-asthana-b6754510a/'} style={{ textDecoration: 'none', color: 'silver', padding: '5px', fontSize: '20px' }} target="_blank" rel="noreferrer">
+                    <a href={'https://www.linkedin.com/in/karan-asthana-b6754510a/'} target="_blank" rel="noreferrer">
                         <i class="ri-linkedin-fill"></i>
                     </a>
-                    <a href={'https://github.com/karanasthana/'} style={{ textDecoration: 'none', color: 'silver', padding: '5px', fontSize: '20px' }} target="_blank" rel="noreferrer">
+                    <a href={'https://github.com/karanasthana/'} target="_blank" rel="noreferrer">
                         <i class="ri-github-fill"></i>
                     </a>
-                    <a href={'https://www.instagram.com/karan_asthana/'} style={{ textDecoration: 'none', color: 'silver', padding: '5px', fontSize: '20px' }} target="_blank" rel="noreferrer">
+                    <a href={'https://www.instagram.com/karan_asthana/'} target="_blank" rel="noreferrer">
                         <i class="ri-instagram-fill"></i>
                     </a>
-                    <a href={'https://www.facebook.com/karan.asthana1/'} style={{ textDecoration: 'none', color: 'silver', padding: '5px', fontSize: '20px' }} target="_blank" rel="noreferrer">
+                    <a href={'https://www.facebook.com/karan.asthana1/'} target="_blank" rel="noreferrer">
                         <i class="ri-facebook-fill"></i>
                     </a>
-                    <a href={'mailto:karan.asthana1@gmail.com'} style={{ textDecoration: 'none', color: 'silver', padding: '5px', fontSize: '20px' }} target="_blank" rel="noreferrer">
+                    <a href={'mailto:karan.asthana1@gmail.com'} target="_blank" rel="noreferrer">
                         <i class="ri-mail-fill"></i>
                     </a>
                 </div>
