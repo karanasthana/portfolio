dfbf333e919c6c5d7874830758a83866e8a2e394
Karan Asthana <karan@p3infotech.in>
2021-02-17 23:12:45 +0530

    moved inline css to less

--- src/Sections/FirstFoldAboutKaran.js
+++ src/Sections/FirstFoldAboutKaran.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import Background from '../assets/white-bg.jpg';
+import './sass.scss';
 
 export default class FirstFoldAboutKaran extends React.Component {
     render() {
-    return (
-        <div style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover' }}>
+      return (
+         <div className="first-fold-section" style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover' }}>
                <div className="container">
                   <div className="row">
-                     <div className="col-12 col-md-6" style={{ display: "flex", justifyContent: "center", flexDirection: "column", marginTop: "20px", marginBottom: "20px" }}>
-                        <h1 style={{ marginBottom: '20px' }}>
-                           Hi, I am <span style={{ color: 'orange' }}>Karan Asthana</span>
+                     <div className="col-12 col-md-6 about-info">
+                        <h1>
+                           Hi, I am <span className="color-orange">Karan Asthana</span>
                         </h1>
-                        <h3 style={{ marginBottom: '20px' }}>
-                           Frontend Developer | Mobile Developer
+                        <h3>
+                           Web Developer | Mobile Developer
                         </h3>
-                        <h5 style={{ fontStyle: 'italic' }}>
+                        <h5>
                            {/* Lorem ipsum dolor sit amet */}
                         </h5>
                      </div>
-                     <div className="col-12 col-md-6" style={{ maxHeight: '80vh', display: "flex", justifyContent: 'center' }}>
+                     <div className="col-12 col-md-6 img-container">
                         <Image src="https://travel-photos-react-website.s3.ap-south-1.amazonaws.com/IMG_8447-removebg-preview-removebg-preview.png" fluid />
                      </div>
                   </div>
                </div>
             </div>
-    );
-}
+      );
+   }
 }
