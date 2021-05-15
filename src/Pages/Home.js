2e65f55e5c79a11b0ede07436ec81030ebc32ba4
Karan Asthana <karan@p3infotech.in>
2021-05-15 22:55:48 +0530

    added one common file for different work experiences

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import FirstFoldAboutKaran from '../Sections/FirstFoldAboutKaran';
 import Footer from '../Sections/Footer';
-import OrgzitWorkEx from '../Sections/OrgzitWorkEx';
-import TCSWorkEx from '../Sections/TCSWorkEx';
+import WorkEx from '../Sections/WorkEx';
 
 class Home extends React.Component {
    render() {
       return (
          <div>
             <FirstFoldAboutKaran />
-            <OrgzitWorkEx />
-            <TCSWorkEx />
+            <WorkEx />
             <Footer />
          </div>
       )
