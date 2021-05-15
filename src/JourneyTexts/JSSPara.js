5a109fd5260c68bd52b9dc7952d4483bd53befe4
Karan Asthana <karan@p3infotech.in>
2021-05-16 02:51:45 +0530

    changed location of Para files

---
+++ src/JourneyTexts/JSSPara.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Sections/sass.scss';
+
+const JSSPara = props => {
+    return (
+        <>
+        <div className="para">
+            <p> JSS is affiliated to the DR APJ Abdul Kalam Technical University </p>
+        </div>
+        <div className="para">
+            <p> My Professional responsibilities here, were - </p>
+            <ul className="reponsibilities">
+                <li>To Develop graphs on the ETL Tool, Ab-initio, to manage and manipulate data for a US-based pharmaceutical giant.</li>
+                <li>Extract data for client requests using Unix commands and running Ab-Initio graphs</li>
+            </ul>
+        </div>
+        </>
+    );
+};
+
+export default JSSPara;
---
+++ src/JourneyTexts/TCSPara.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Sections/sass.scss';
+
+const TCSPara = props => {
+    return (
+        <>
+        <div className="para">
+            <p> Tata Consultancy Services (TCS) is one of the biggest multinational company from India, employing more than 4Lakh people across the world. </p>
+        </div>
+        <div className="para">
+            <p> My Professional responsibilities here, were - </p>
+            <ul className="reponsibilities">
+                <li>To Develop graphs on the ETL Tool, Ab-initio, to manage and manipulate data for a US-based pharmaceutical giant.</li>
+                <li>Extract data for client requests using Unix commands and running Ab-Initio graphs</li>
+            </ul>
+        </div>
+        </>
+    );
+};
+
+export default TCSPara;
