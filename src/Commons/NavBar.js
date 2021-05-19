499a6469402527ab9882918743c1bc2acb386f7d
Karan Asthana <karan@p3infotech.in>
2021-05-20 00:15:20 +0530

    added changes to effect react-scroll

--- src/Commons/NavBar.js
+++ src/Commons/NavBar.js
@@ -4,17 +4,48 @@ import Nav from 'react-bootstrap/Nav';
 import './NavBar.scss';
 import * as CONSTANTS from '../constants/constants';
 import { Button } from '../StyledComponents/Button';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 class NavBar extends React.Component {
    render() {
       return (
          <Navbar bg="light" expand="lg" className="navbar-style">
-            <Navbar.Brand href="#home">Karan Asthana</Navbar.Brand>
+            <Navbar.Brand href="#home">
+               <Link
+                  activeClass="active-link"
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}>
+                  Karan Asthana
+               </Link>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="reverse-row-nav" >
                <Nav>
-                  <Nav.Link href="#industry">Industry Experience</Nav.Link>
-                  <Nav.Link href="#projects">Projects</Nav.Link>
+                  <Nav.Link href="#industry">
+                     <Link
+                        activeClass="active-link"
+                        to="work-ex"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}>
+                        Industry Experience
+                     </Link>
+                  </Nav.Link>
+                  <Nav.Link href="#projects">
+                     <Link
+                        activeClass="active-link"
+                        to="projects"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}>
+                        Projects
+                     </Link>
+                  </Nav.Link>
                   <Nav.Link href="#contact">Contact Me</Nav.Link>
                   <Nav.Link href={CONSTANTS.RESUME_PDF_LINK} target="_blank" rel="noreferrer" download>
                      <Button primary className="download-btn">Download Resume</Button>
