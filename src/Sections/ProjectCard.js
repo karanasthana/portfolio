f46241b5fbb655425f7391820fee6de142fe5525
Karan Asthana <karan.asthana1@gmail.com>
2021-06-04 17:54:47 +0530

    made changes to ProjectCard and Projects

--- src/Sections/ProjectCard.js
+++ src/Sections/ProjectCard.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import './sass.scss';
+import { Modal } from 'react-bootstrap';
+// import Image from 'react-bootstrap/Image';
+import { Button } from '../StyledComponents/Button';
 
 export default function ProjectCard(props) {
+    const [showDialog, setShowDialog] = useState(false);
+
+    const handleClose = () => setShowDialog(false);
+    const handleShow = () => setShowDialog(true);
+
     return (
-        // <div className="col-12 col-sm-6 col-md-4 col-lg-3" style={{ padding: '20px' }}>
-        <Card className="cardContainer" onClick={() => console.log('click')}>
-            <div className="cardOuterDiv">
-                <Card.Img variant="top" src={`${props.image}`} className="cardImg" />
-                <Card.Body className="cardBody">
-                    <Card.Title className="cardTitle">{`${props.name}`}</Card.Title>
-                    <Card.Text>{props.description}</Card.Text>
-                </Card.Body>
-            </div>
-        </Card>
-        // </div>
+        <>
+            <Card className="cardContainer" onClick={() => handleShow()}>
+                <div className="cardOuterDiv">
+                    <Card.Img variant="top" src={`${props.image}`} className="cardImg" />
+                    <Card.Body className="cardBody">
+                        <Card.Title className="cardTitle">{`${props.name}`}</Card.Title>
+                        <Card.Text>{props.description}</Card.Text>
+                    </Card.Body>
+                </div>
+            </Card>
+            <Modal show={showDialog} onHide={handleClose} style={{ zIndex: '2000' }} size="lg">
+                <Modal.Header closeButton className='modal-header'>
+                    <Modal.Title>
+                        {props.name}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {props.detailUI}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
