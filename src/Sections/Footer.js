1f671f34d6e1f7dcf4a0ceecc33e678d8a9c844b
Karan Asthana <karan.asthana1@gmail.com>
2021-06-05 00:42:44 +0530

    added image to the footer to increase height :P

--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -2,40 +2,50 @@ import React from 'react';
 import { Button } from '../StyledComponents/Button';
 import './sass.scss';
 import * as CONSTANTS from '../constants/constants';
+import Image from 'react-bootstrap/Image';
 
 export default class Footer extends React.Component {
     render() {
         return <div className="footer-section">
-            <div className="pdf-row">
-                <Button primary>
-                    <a href={CONSTANTS.RESUME_PDF_LINK} target="_blank" rel="noreferrer" download style={{  }}>
-                        PDF Resume
-                    </a>
-                </Button>
-            </div>
-            <div className="footer-icons-row">
-                <div className="copyright-container">
-                    Copyright <i className ="ri-copyright-line" /> {' Karan Asthana'} All rights reserved.
+            <div className="container">
+                <div className="pdf-row">
+                    <Button primary>
+                        <a href={CONSTANTS.RESUME_PDF_LINK} target="_blank" rel="noreferrer" download style={{  }}>
+                            PDF Resume
+                        </a>
+                    </Button>
                 </div>
-                <div className="row icons-container">
-                    <a href={CONSTANTS.TRAVEL_LINK} target="_blank" rel="noreferrer">
-                        <i className="ri-earth-fill"></i>
-                    </a>
-                    <a href={CONSTANTS.LINKEDIN_LINK} target="_blank" rel="noreferrer">
-                        <i className="ri-linkedin-fill"></i>
-                    </a>
-                    <a href={CONSTANTS.GITHUB_LINK} target="_blank" rel="noreferrer">
-                        <i className="ri-github-fill"></i>
-                    </a>
-                    <a href={CONSTANTS.INSTAGRAM_LINK} target="_blank" rel="noreferrer">
-                        <i className="ri-instagram-fill"></i>
-                    </a>
-                    <a href={CONSTANTS.FACEBOOK_LINK} target="_blank" rel="noreferrer">
-                        <i className="ri-facebook-fill"></i>
-                    </a>
-                    <a href={CONSTANTS.EMAILTO_LINK} target="_blank" rel="noreferrer">
-                        <i className="ri-mail-fill"></i>
-                    </a>
+                <div className="footer-icons-row">
+                    <div className="row icons-container col-xs-12 col-sm-6 ">
+                        <a href={CONSTANTS.TRAVEL_LINK} target="_blank" rel="noreferrer">
+                            <i className="ri-earth-fill"></i>
+                            My Travel Scrapbook
+                        </a>
+                        <a href={CONSTANTS.LINKEDIN_LINK} target="_blank" rel="noreferrer">
+                            <i className="ri-linkedin-fill"></i>
+                            LinkedIn
+                        </a>
+                        <a href={CONSTANTS.GITHUB_LINK} target="_blank" rel="noreferrer">
+                            <i className="ri-github-fill"></i>
+                            Github
+                        </a>
+                        <a href={CONSTANTS.INSTAGRAM_LINK} target="_blank" rel="noreferrer">
+                            <i className="ri-instagram-fill"></i>
+                            Instagram
+                        </a>
+                        <a href={CONSTANTS.FACEBOOK_LINK} target="_blank" rel="noreferrer">
+                            <i className="ri-facebook-fill"></i>
+                            Facebook
+                        </a>
+                        <a href={CONSTANTS.EMAILTO_LINK} target="_blank" rel="noreferrer">
+                            <i className="ri-mail-fill"></i>
+                            Contact Me
+                        </a>
+                    </div>
+                    <div className="copyright-container col-xs-12 col-sm-6 ">
+                        <span>Copyright <i className ="ri-copyright-line" /> Karan Asthana All rights reserved.</span>
+                        <Image src={CONSTANTS.KARAN_DP_IMG_LINK} rounded fluid className="footer-img-karan" />
+                    </div>
                 </div>
             </div>
         </div>
