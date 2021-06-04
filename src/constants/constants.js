083b56118eec2b427320c0194d8573124c97cda4
Karan Asthana <karan.asthana1@gmail.com>
2021-06-04 23:40:08 +0530

    moved all files links to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,16 +6,40 @@ export const INSTAGRAM_LINK = 'https://www.instagram.com/karan_asthana/';
 export const FACEBOOK_LINK = 'https://www.facebook.com/karan.asthana1/';
 export const EMAILTO_LINK = 'mailto:karan.asthana1@gmail.com';
 
+
 // links github repos
 export const TRAVEL_GITHUB = "https://github.com/karanasthana/travel";
+export const VACCINE_GITHUB = "https://github.com/karanasthana/SlotScheduler";
+export const JAVASCRIPTPREP_GITHUB = "https://github.com/karanasthana/JavascriptPrep";
+export const MORSECODE_GITHUB = "https://github.com/karanasthana/morse-code-website";
 
-export const RESUME_PDF_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/Karan+Asthana+Resume+-+Frontend+Developer+-+React+Native+.pdf';
 
 // links Karan s3
+export const RESUME_PDF_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/Karan+Asthana+Resume+-+Frontend+Developer+-+React+Native+.pdf';
+
 export const KARAN_IMG = 'https://travel-photos-react-website.s3.ap-south-1.amazonaws.com/IMG_8447-removebg-preview-removebg-preview.png';
 export const TRAVEL_WEBSITE_SS_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/travel.png';
 export const VACCINE_PROBLEM_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/vaccine-problem-img.jpeg';
 
+export const VACCINE_PROJ_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/vaccine.jpeg';
+export const TRAVEL_PROJ_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/Screen+Shot+2021-05-16+at+5.50.17+PM.png';
+export const JAVASCRIPTPREP_PROJ_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/js.jpeg';
+export const MORSECODE_PROJ_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/morse-code.png';
+export const CRT_PROJ_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/crt.jpeg';
+export const RAIL_PROJ_IMG_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/indian_rail.jpeg';
+
 
 // links random
 export const YJHD_TRAVEL_BOOK_VIDEO = 'https://youtu.be/ShQg9Yz25ZA?t=88';
+
+
+// links work ex
+export const UFL_IMAGE_LINK = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/University_of_Florida_logo.svg/1280px-University_of_Florida_logo.svg.png';
+export const ORGZIT_IMAGE_LINK = 'https://d3a93fg1wt2nf3.cloudfront.net/static/website/images/orgzit_logo.png';
+export const TCS_IMAGE_LINK = 'https://www.cdn.geeksforgeeks.org/wp-content/uploads/TATA_Consultancy_Services_Logo_blue.svg_.png';
+export const JSS_IMAGE_LINK = 'https://jssaten.ac.in/img/JSS_Logo.png';
+
+// links Orgzit
+export const ORGZIT_LOGO_IMAGE_LINK = 'https://media-exp1.licdn.com/dms/image/C4D0BAQFXmjcLugUNsQ/company-logo_200_200/0/1519906878959?e=2159024400&v=beta&t=o7ayzYc-kbvEXZSlCF6e8UKficNlzVsXtd5omtk9ww8';
+export const APPLE_STORE_ORGZIT_LINK = 'https://apps.apple.com/gb/app/orgzit/id1504448187';
+export const GOOGLE_PLAYSTORE_ORGZIT_LINK = 'https://play.google.com/store/apps/details?id=com.orgzit.android.pro&hl=en_IN&gl=US';
