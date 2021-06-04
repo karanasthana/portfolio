5d7c08dca82085673c1f6fbcec0e373e2e1c7798
Karan Asthana <karan.asthana1@gmail.com>
2021-06-04 23:08:45 +0530

    moved links og TravelScrapbook

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
+// links footer
 export const TRAVEL_LINK = 'https://karanasthana.github.io/travel/';
 export const GITHUB_LINK = 'https://github.com/karanasthana/';
 export const LINKEDIN_LINK = 'https://www.linkedin.com/in/karan-asthana-b6754510a/';
@@ -5,6 +6,14 @@ export const INSTAGRAM_LINK = 'https://www.instagram.com/karan_asthana/';
 export const FACEBOOK_LINK = 'https://www.facebook.com/karan.asthana1/';
 export const EMAILTO_LINK = 'mailto:karan.asthana1@gmail.com';
 
+// links github repos
+export const TRAVEL_GITHUB = "https://github.com/karanasthana/travel";
+
 export const RESUME_PDF_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/Karan+Asthana+Resume+-+Frontend+Developer+-+React+Native+.pdf';
 
+// links Karan s3
 export const KARAN_IMG = 'https://travel-photos-react-website.s3.ap-south-1.amazonaws.com/IMG_8447-removebg-preview-removebg-preview.png';
+export const TRAVEL_WEBSITE_SS_LINK = 'https://karanpersonal.s3.ap-south-1.amazonaws.com/travel.png';
+
+// links random
+export const YJHD_TRAVEL_BOOK_VIDEO = 'https://youtu.be/ShQg9Yz25ZA?t=88';
