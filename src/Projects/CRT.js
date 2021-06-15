bd3dac24ded5c09506005103339bd8ad7c42e2e9
Karan Asthana <karan.asthana1@gmail.com>
2021-06-15 17:45:48 +0530

    moved abstract to the end

--- src/Projects/CRT.js
+++ src/Projects/CRT.js
@@ -4,10 +4,7 @@ export default function TravelScrapbook(props) {
     return (
         <div>
             <p>
-                Continuous Rail Temperature Recorder is a versatile device specially designed for continuously displaying Rail Temperature, Storing Data, without a requirement of any external PC or Laptop to transfer the data to the rail Server, it can transfer data automatically when it has internet connectivity (on the device itself). The device is attached with an internal backup battery which enables the recorder to continuously record & display the data in case of power failure up to 72 hours. The Flash drive can record the data up to 5 years on per minute basis. The device comes with inbuilt USB port for data transfer and 3.5mm Port for communication with telemetry devices. It has a beautiful, compact and very sleek design. It has Special Safety alarms on MAX/MIN Crossover & Sensor Disconnection, making this device fully secure. The device has the feature of showing the Hourly Maximum/Minimum Temperature in real time. Device is also User Password protected. 
-            </p>
-            <p>
-                Salient Features
+                <strong>Salient Features</strong>
             </p>
             <ul>
                 <li>More storage with 16 GB inbuilt Storage which can store data up to 5 years on per minute basis.</li>
@@ -18,6 +15,14 @@ export default function TravelScrapbook(props) {
                 <li>Customized selection of Time Interval for Recorded data Retrieval.</li>
                 <li>Customized Time and Date selection for Auto Start recording. </li>
             </ul>
+
+            <p>
+                <strong>Abstract</strong>
+            </p>
+
+            <p>
+                Continuous Rail Temperature Recorder is a versatile device specially designed for continuously displaying Rail Temperature, Storing Data, without a requirement of any external PC or Laptop to transfer the data to the rail Server, it can transfer data automatically when it has internet connectivity (on the device itself). The device is attached with an internal backup battery which enables the recorder to continuously record & display the data in case of power failure up to 72 hours. The Flash drive can record the data up to 5 years on per minute basis. The device comes with inbuilt USB port for data transfer and 3.5mm Port for communication with telemetry devices. It has a beautiful, compact and very sleek design. It has Special Safety alarms on MAX/MIN Crossover & Sensor Disconnection, making this device fully secure. The device has the feature of showing the Hourly Maximum/Minimum Temperature in real time. Device is also User Password protected. 
+            </p>
         </div>
     );
 }
