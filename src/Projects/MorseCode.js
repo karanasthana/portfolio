5fd519f790e5fb539228272796a8dc680ace4a93
Karan Asthana <karan.asthana1@gmail.com>
2021-06-16 22:33:53 +0530

    added files for rdps and morsecode

---
+++ src/Projects/MorseCode.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function MorseCode(props) {
+    return (
+        <div>
+            <p>
+                <strong>Salient Features</strong>
+            </p>
+            <ul>
+                <li>More storage with 16 GB inbuilt Storage which can store data up to 5 years on per minute basis</li>
+                <li>Having inbuilt internet Connectivity solution (Ethernet, Wi-Fi, Bluetooth) to transfer files</li>
+                <li>Very Compaq, beautiful and very sleek design</li>
+                <li>Record Temp at one minute Interval</li>
+                <li>Real Time Temp Tracking (Live, Min-Max Temp With time )</li>
+                <li>Customized selection of Time Interval for Recorded data Retrieval</li>
+                <li>Customized Time and Date selection for Auto Start recording </li>
+            </ul>
+
+            <p>
+                <strong>Abstract</strong>
+            </p>
+
+            <p>
+                Continuous Rail Temperature Recorder is a versatile device specially designed for continuously displaying Rail Temperature, Storing Data, without a requirement of any external PC or Laptop to transfer the data to the rail Server, it can transfer data automatically when it has internet connectivity (on the device itself). The device is attached with an internal backup battery which enables the recorder to continuously record & display the data in case of power failure up to 72 hours. The Flash drive can record the data up to 5 years on per minute basis. The device comes with inbuilt USB port for data transfer and 3.5mm Port for communication with telemetry devices. It has a beautiful, compact and very sleek design. It has Special Safety alarms on MAX/MIN Crossover & Sensor Disconnection, making this device fully secure. The device has the feature of showing the Hourly Maximum/Minimum Temperature in real time. Device is also User Password protected. 
+            </p>
+        </div>
+    );
+}
