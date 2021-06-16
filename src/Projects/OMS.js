8584691d68b06c5ca4c49e1b97e48b401b15fc9f
Karan Asthana <karan.asthana1@gmail.com>
2021-06-16 10:40:53 +0530

    added the OMS file

---
+++ src/Projects/OMS.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function OMS(props) {
+    return (
+        <div>
+            <p>
+                <strong>Salient Features</strong>
+            </p>
+            <ul>
+                <li>High accuracy-3m to 5m</li>
+                <li>Report details viewed in system</li>
+                <li>No requirement of Internet connectivity</li>
+                <li>Compatible to retrieve Execution, Exception Report & Ride Index</li>
+                <li>Provision of real time SMS/E-MAIL alert to concerned officials for peaks above thresholds limits</li>
+                <li>Event marker app included to record various track features</li>
+                <li>Highly sensitive acceleration sensors</li>
+                <li>Easy in handling and in use</li>
+                <li>Battery backup up to 8hrs</li>
+                <li>Compact, Smart design and fast processing</li>
+                <li>64 GB of internal memory and extendable up to 128gb</li>
+            </ul>
+
+            <p>
+                <strong>Abstract</strong>
+            </p>
+
+            <p>
+                Android GPS based Oscillation Measurement System intends to record the Vertical and Lateral accelerations of Railway Rolling Stock, running on the track digitally. It receives the locations details from a Geo Positioning System (GPS) which is inbuilt in android tablet, and records the vertical & lateral accelerations along with location. This system has 2 modes, GPS mode and time mode. It also has facility to record the TMS details & print on-line execution & exception report, ride index. 
+            </p>
+        </div>
+    );
+}
